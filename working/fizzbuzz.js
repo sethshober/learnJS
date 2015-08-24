f5f85eb9b4b5a697720bdf30d9fb22a74bf78067
Michael_Montero <mmontero2012@gmail.com>
2015-08-23 18:57:40 -0600

    added fizz buzz file and exmplanation

---
+++ working/fizzbuzz.js
@@ -0,0 +1,7 @@
+//Fizz Buzz is a famous functional programming excersize that usesmmathematics and conditionals common to all languages. 
+//The Problem:
+///Write a program that prints the numbers from 1 to 100. 
+//For multiples of three print “Fizz” instead of the number 
+//For the multiples of five print “Buzz”. 
+//For numbers which are multiples of both three and five print “FizzBuzz”
+//We will provide you with a few different solutions as there are many different ways to skin a cat.
